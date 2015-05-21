133bd23ac702767a526d2bc088ea6dec8cf4f004
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2015-05-21 19:22:45 +0200

    Nicer error messages

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,13 @@ fn main () {
   let args : Vec<String> = std::env::args().collect();
   if args.len() == 2 {
     println!("Reading file {}...", args[1]);
-    let mut file = (File::open(&args[1])).unwrap();
+    let mut file = match File::open(&args[1]) {
+      Err (e) => {
+        println!("Fatal error: {}", e);
+        return ();
+      },
+      Ok (f) => f,
+    };
     let mem = ocaml::marshal::read_file(&mut file);
     println!("{:?}", mem);
   } else {
