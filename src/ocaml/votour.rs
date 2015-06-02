88a2942fd854a5763f1aced3b63c53baa20e262e
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2015-06-02 21:09:57 +0200

    Computation of used memory

--- src/ocaml/votour.rs
+++ src/ocaml/votour.rs
@@ -1,23 +1,39 @@
 use ocaml::marshal::*;
 
-fn object_size(obj : &Obj) -> u64 {
-  match *obj {
+fn loop_size(mem : &[Obj], ans : &mut[u64], off : usize) -> u64 {
+  let mut size = ans[off];
+  if size > 0 { return size; };
+  match mem[off] {
     Obj::Block (_, ref args) => {
-      1 + args.len() as u64
+      let len = args.len();
+      size = size + 1 + len as u64;
+      for _ in 0..len {
+        match args[len] {
+          Field::Ref (off) => {
+            size = size + loop_size(mem, ans, off);
+          },
+          Field::Int (..) => (),
+          Field::Abs (..) => (),
+          Field::Atm (..) => (),
+        }
+      }
     },
     Obj::String (ref s) => {
       let len = s.len() as u64;
-      1 + len / 8
+      size = size + 1 + len / 8;
     },
   }
+  ans[off] = size;
+  return size;
 }
 
-fn compute_size(mem : &Memory) -> Box<[u64]> {
-  let Memory(ref mem) = *mem;
+fn compute_size(mem : &[Obj]) -> Box<[u64]> {
   let len = mem.len();
   let mut ans = Vec::with_capacity(len);
-  for i in 0..len {
+  for _ in 0..len {
     ans.push(0);
   };
-  ans.into_boxed_slice()
+  let mut ans = ans.into_boxed_slice();
+  let _ = loop_size(mem, &mut ans, 0);
+  ans
 }
